66f839c9d7b5b9aecf070937e4112b81951a3765
vidurham <vdurham@prideseals.com>
2022-06-07 15:14:01 -0400

    updated galleries

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
