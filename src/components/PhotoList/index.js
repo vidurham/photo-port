eb9efbb8059a125366627448e5becc8abe16b07a
vidurham <vdurham@prideseals.com>
2022-06-07 18:54:52 -0400

    picture modals made

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
     const [photos] = useState([
@@ -23,16 +24,27 @@ const PhotoList = ({ category }) => {
         {name: 'Park bench', category: 'landscape', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie'}
     ]);
 
+    const [currentPhoto,setCurrentPhoto] = useState();
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     const currentPhotos = photos.filter(photo => photo.category === category);
+
+    const toggleModal = (image, i) => {
+        setCurrentPhoto({...image, index: i})
+        setIsModalOpen(true);
+    }
 
     return (
         <div>
+            {isModalOpen && <Modal currentPhoto={currentPhoto} />}
             <div className='flex-row'>
                 {currentPhotos.map((image, i) => (
                     <img 
                     src={require(`../../assets/small/${category}/${i}.jpg`)}
                     alt={image.name}
                     className='img-thumbnail mx-1'
+                    onClick={() => toggleModal(image, i)}
                     key={image.name}
                     />
                 ))}
