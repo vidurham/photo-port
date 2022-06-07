e369c00e036e5b8285967e3c424fd784d0c5cdbe
vidurham <vdurham@prideseals.com>
2022-06-07 15:39:32 -0400

    added contact form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from "./components/Gallery";
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -21,6 +22,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
